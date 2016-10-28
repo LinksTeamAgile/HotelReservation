19489bf371e7eb2e59d04c0e03d3e990892de4bf
Antonello Giordano <johnnyupset@gmail.com>
2016-10-28 18:54:40 +0200

    Delete Room / Delete Customer / Delete Reservation

--- src/com/links/ressys/Sys.java
+++ src/com/links/ressys/Sys.java
@@ -1,11 +1,12 @@
 package com.links.ressys;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import com.links.ressys.core.Customer;
 import com.links.ressys.core.Reservation;
 import com.links.ressys.core.Room;
-
 import com.links.ressys.core.RoomConcrete;
 
 public class Sys {
@@ -19,10 +20,65 @@ public class Sys {
 		roomList.add(roomCrt1);
 	}
 	
-	
 	public void createRoom() {
 		
 	}
+	
+	public boolean deleteRoom(int roomId){
+		boolean roomRemoved = false;
+		for(Room r: this.roomList){
+			if(r.getRoomId()==roomId){
+				roomList.remove(r);
+				roomRemoved = true;
+			}
+		}
+		if(roomRemoved){
+			System.out.println("Room "+roomId+" deleted");
+			return roomRemoved;
+		}
+		else{
+			System.out.println("Room not exists");
+			return roomRemoved;
+		}
+	}
+	
+	public boolean deleteCustomer(String mailAddress){
+		boolean customerRemoved = false;
+		for(Customer c: this.customerList){
+			if(c.getMailAddress()==mailAddress){
+				customerList.remove(c);
+				customerRemoved = true;
+			}
+		}
+		if(customerRemoved){
+			System.out.println("Customer with "+mailAddress+" address deleted");
+			return customerRemoved;
+		}
+		else{
+			System.out.println("Customer not exists");
+			return customerRemoved;
+		}
+			
+	}
+	
+	public boolean deleteReservation(int reservationId){
+		boolean reservationRemoved = false;
+		for(Reservation rs: this.reservationList){
+			if(rs.getReservationId()==reservationId){
+				reservationList.remove(rs);
+				reservationRemoved = true;
+			}
+		}
+		if(reservationRemoved){
+			System.out.println("Reservation "+reservationId+" deleted");
+			return reservationRemoved;
+		}
+		else{
+			System.out.println("Reservation not exists");
+			return reservationRemoved;
+		}
+	}
+
 	
 	public void showRoom() {
 		for(Room r: this.roomList){
