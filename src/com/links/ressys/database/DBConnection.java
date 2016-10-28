1daffe96294c4e435af1605dc600763b9b7c2453
Mirko Russo <mirkorusso28@gmail.com>
2016-10-28 11:29:53 +0200

    Hotfixes
    Note: JDBC driver required

--- src/com/links/ressys/database/DBConnection.java
+++ src/com/links/ressys/database/DBConnection.java
@@ -5,54 +5,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import org.sqlite.*;
-import org.sqlite.jdbc4.JDBC4Connection;
-
-import com.links.ressys.core.*;
-
 public class DBConnection {
 
 	private String URL = "jdbc:sqlite:/Documents/GitHub/HotelReservation/res/HotelReservation.sqlite";
-	private String userName = "";
-	private String password = "";
 
-	
-	public DBConnection() {
-		// TODO Auto-generated constructor stub
-	}
-	public static void main (String[] args) {
+	public static void main (String[] args) throws Exception {
 		DBConnection dbc = new DBConnection();
-		try {
 			dbc.getRooms();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
-	public Room[] getRooms() throws Exception{
+	public void getRooms() throws Exception{
+		String query = "SELECT * FROM room";
 		String sDriverName = "org.sqlite.JDBC";
 		Class.forName(sDriverName);
 		
-		String query = "SELECT * FROM room";
 		try(Connection con = DriverManager.getConnection(URL);
 				Statement stmt = con.createStatement();
-				ResultSet rs = stmt.executeQuery(query)){
+				ResultSet rs = stmt.executeQuery(query)) {
+			
 			while(rs.next()) {
 				System.out.println(rs.getInt("idRoom"));
 				System.out.println(rs.getInt("isServiceable"));
 				System.out.println(rs.getInt("isAvailable"));
 				System.out.println(rs.getInt("maxGuest"));
 				System.out.println(rs.getString("services"));
-				System.out.println("____________________________");
-
+				System.out.println("- - - - - - - - - - - - - -");
 			}
-			
-			
+					
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return null;
-		
 	}
 }
