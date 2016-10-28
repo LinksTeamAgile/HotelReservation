482c2a2678483e12d66ff7770d9af7a076acbae9
Mirko Russo <mirkorusso28@gmail.com>
2016-10-28 12:45:56 +0200

    DBConnection updated

--- src/com/links/ressys/database/DBConnection.java
+++ src/com/links/ressys/database/DBConnection.java
@@ -4,16 +4,18 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.links.ressys.core.Room;
+import com.links.ressys.core.RoomConcrete;
 
 public class DBConnection {
 
 	private String URL = "jdbc:sqlite:/Documents/GitHub/HotelReservation/res/HotelReservation.sqlite";
 
-	public static void main (String[] args) throws Exception {
-		DBConnection dbc = new DBConnection();
-			dbc.getRooms();
-	}
-	public void getRooms() throws Exception{
+	ArrayList<Room> room = new ArrayList<Room>();
+
+	public ArrayList<Room> getRooms() throws Exception{
 		String query = "SELECT * FROM room";
 		String sDriverName = "org.sqlite.JDBC";
 		Class.forName(sDriverName);
@@ -23,16 +25,18 @@ public class DBConnection {
 				ResultSet rs = stmt.executeQuery(query)) {
 			
 			while(rs.next()) {
-				System.out.println(rs.getInt("idRoom"));
+				room.add(new RoomConcrete(rs.getInt("idRoom"), rs.getBoolean("isServiceable"), 
+						rs.getBoolean("isAvailable"), rs.getInt("maxGuest"), rs.getString("services").split(" ")));
+				/*System.out.println(rs.getInt("idRoom"));
 				System.out.println(rs.getInt("isServiceable"));
 				System.out.println(rs.getInt("isAvailable"));
 				System.out.println(rs.getInt("maxGuest"));
 				System.out.println(rs.getString("services"));
-				System.out.println("- - - - - - - - - - - - - -");
-			}
-					
+				System.out.println("- - - - - - - - - - - - - -");*/
+			}		
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		return room;		
 	}
 }
