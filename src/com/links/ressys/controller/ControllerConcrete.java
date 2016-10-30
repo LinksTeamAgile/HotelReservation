e7e2c8aa251ec8302f73a8c4dd440527f8cc9068
Gianlucadv <gianlucadelvecchio89@gmail.com>
2016-10-30 13:37:45 +0100

    Call show-functions of Sys on ControllerConcrete.

--- src/com/links/ressys/controller/ControllerConcrete.java
+++ src/com/links/ressys/controller/ControllerConcrete.java
@@ -40,7 +40,7 @@ public class ControllerConcrete extends Controller {
 				System.out.println("method for DeleteRoom");
 				break;
 			case 6: // showCustomers(,...)
-				System.out.println("method for ShowRooms");
+				super.sys.showCustomer();;
 				break;
 			case 7: // createReservation(,...)
 				System.out.println("method for CreateReservation");
@@ -49,7 +49,7 @@ public class ControllerConcrete extends Controller {
 				System.out.println("method for DeleteReservation");
 				break;
 			case 9: // showReservation(,...)
-				System.out.println("method for ShowRooms");
+				super.sys.showReservation();
 				break;
 			case 0: // System.exit(0)
 				System.out.println("Exit!");
