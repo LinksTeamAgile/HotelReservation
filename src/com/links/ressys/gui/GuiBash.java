a0e437c92418289cce8974a3f0408a3700bb432c
drlc <daraloca@live.it>
2016-11-01 18:39:13 +0100

    make try-resources

--- src/com/links/ressys/gui/GuiBash.java
+++ src/com/links/ressys/gui/GuiBash.java
@@ -1,20 +1,21 @@
 package com.links.ressys.gui;
 
-import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class GuiBash implements Gui {
-	
-	private void viewMenu(){
+
+	@Override
+	public void viewMenu(){
 		System.out.println("Menu'");
 		System.out.println("1 - Create room.");
-		System.out.println("2 - Delete room.");
+//		System.out.println("2 - Delete room.");
 		System.out.println("3 - Show rooms.");
 		System.out.println("4 - Create customer.");
-		System.out.println("5 - Delete customer.");
+//		System.out.println("5 - Delete customer.");
 		System.out.println("6 - Show customers.");
 		System.out.println("7 - Create reservation.");
-		System.out.println("8 - Modify reservation.");
+//		System.out.println("8 - Modify reservation.");
 		System.out.println("9 - Show reservations.");
 		System.out.println("0 - Exit");
 	}
@@ -28,21 +29,16 @@ public class GuiBash implements Gui {
 	public void close() {
 	}
 
-	@SuppressWarnings("resource")
 	@Override
 	public String getInput(String message) {
-		Scanner keyboard = new Scanner(System.in);
-		int scanneredNumber = 0;		
-
-		System.out.println(message);
-		try {
-			scanneredNumber = keyboard.nextInt();
-		} catch (InputMismatchException ex) {
+		try(Scanner keyboard = new Scanner(System.in)){
+			System.out.print(message);
+			return keyboard.next();
+		} catch (NoSuchElementException ex) {
 			System.out.println("User input is not a valid value for this method.");
 			System.out.println("Exception caught: User cannot put that value as menu choice.");
-			scanneredNumber = -1;
+			return "-1";
 		}
-		return Integer.toString(scanneredNumber);
 	}
 
 }
