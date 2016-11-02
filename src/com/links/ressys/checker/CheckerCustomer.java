11fbaad0a0e43b039cea9bb0b4ff8e48a0e9ce85
userm07 <userm07@192.168.12.198>
2016-11-02 16:23:41 +0100

    checkMailAddress - updated

--- src/com/links/ressys/checker/CheckerCustomer.java
+++ src/com/links/ressys/checker/CheckerCustomer.java
@@ -1,84 +1,90 @@
 package com.links.ressys.checker;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.links.ressys.core.Customer;
 import com.links.ressys.statuscodes.*;
 
-public class CheckerCustomer implements Checker{
+public class CheckerCustomer implements Checker {
 
 	private Customer customer;
-	
-	public CheckerCustomer(Customer c){
+
+	public CheckerCustomer(Customer c) {
 		this.customer = c;
 	}
-	
+
 	@Override
 	public ArrayList<Integer> check() {
 		ArrayList<Integer> check = new ArrayList<Integer>();
 		check.add(checkName());
-	    check.add(checkSurname());
-	    check.add(checkTaxCode());
-	    check.add(checkCellPhoneNumber());
-	    check.add(checkMailAddress());
-	    check.add(checkCardNumber());
-	    return check;	
+		check.add(checkSurname());
+		check.add(checkTaxCode());
+		check.add(checkCellPhoneNumber());
+		check.add(checkMailAddress());
+		check.add(checkCardNumber());
+		return check;
 	}
-	
-	private int checkName(){
-		if(customer.getName().equals(""))
+
+	private int checkName() {
+		if (customer.getName().equals(""))
 			return 511;
-		else if(customer.getName().length()<255)
+		else if (customer.getName().length() < 255)
 			return 512;
-		else 
+		else
 			return 100;
 	}
-	
-	private int checkSurname(){
-		if(!customer.getSurname().equals(""))
+
+	private int checkSurname() {
+		if (!customer.getSurname().equals(""))
 			return 521;
-		else if(customer.getSurname().length()<255)
+		else if (customer.getSurname().length() < 255)
 			return 522;
 		else
 			return 100;
 	}
-	/**else{
-		for(int i=0; i<5;i++){
-			if(customer.getTaxCode().charAt(i)<'A' && customer.getTaxCode().charAt(i)>'Z'){
-				return 532;
-			}
-		}
-	}	**/	
-	private int checkTaxCode(){
-		if(!customer.getTaxCode().equals("") )
+
+	/**
+	 * else{ for(int i=0; i<5;i++){ if(customer.getTaxCode().charAt(i)<'A' &&
+	 * customer.getTaxCode().charAt(i)>'Z'){ return 532; } } }
+	 **/
+	private int checkTaxCode() {
+		if (!customer.getTaxCode().equals(""))
 			return 531;
-		else if(customer.getTaxCode().length()!=16)
-			return 532;	
+		else if (customer.getTaxCode().length() != 16)
+			return 532;
 		else
 			return 100;
 	}
-	
-	private int checkCellPhoneNumber(){
-		if(!customer.getCellPhoneNumber().equals(""))
+
+	private int checkCellPhoneNumber() {
+		if (!customer.getCellPhoneNumber().equals(""))
 			return 541;
-		try{
+		try {
 			Integer.parseInt(customer.getCellPhoneNumber());
 			return 100;
-		}catch(NumberFormatException e){
+		} catch (NumberFormatException e) {
 			return 542;
 		}
 	}
-	
-	private int checkMailAddress(){
-		if(!customer.getMailAddress().equals(""))
+
+	private int checkMailAddress() {
+		if (!customer.getMailAddress().equals(""))
+			return 551;
+		String regex = "^(.+)@(.+)$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(customer.getMailAddress());
+		if(!matcher.matches())
+			return 552;
+		else 
 			return 100;
-		else
-			return CustomerCode.EMPTY_MAIL.getCode();
 	}
-	
-	private int checkCardNumber(){
-		if(!customer.getCardNumber().equals(""))
-			return 100;
+
+	private int checkCardNumber() {
+		if (!customer.getCardNumber().equals(""))
+			return 561;
 		else
-			return CustomerCode.EMPTY_CARDNUM.getCode();
+			return 100;
 	}
 }
